33bca8f743843036800ef92bb59611252a81c707
guiguiw <zhanglj35@MAIL2.SYSU.EDU.CN>
2016-10-15 11:51:02 +0800

    Lab1

---
+++ Lab1/app/src/main/java/j/l/lab1/MainActivity.java
@@ -0,0 +1,14 @@
+package j.l.lab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
